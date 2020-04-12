b52283667adc0d1edb40602cdadb43b86d2b3428
Cailenys <cailenys.salazar@gmail.com>
2020-04-12 16:14:24 -0600

    Login form and button functionality were added

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class LoginForm extends React.Component {
+    render () {
+        return (
+             <div>
+                <h1 style ={{color: this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <form>
+                    <div>
+                        <label id="name"></label>
+                        <input type="text" id="username" placeholder="Enter your username"></input>
+                    </div>
+                            
+                    <div>
+                        <label id="user-password"></label>
+                        <input type="password" id="password" placeholer="Enter your password"></input>
+                    </div>       
+                                                
+                </form>
+            </div>
+        )
+    }
+}
+
+class Login extends React.Component {
+    state = {
+        loggedIn: false
+    }
+  
+    logIn = () =>  this.setState({loggedIn: true})
+    logOut = () =>  this.setState({loggedIn: false})
+
+    render () {
+      return (
+          <div>
+            <button onClick =  {this.logIn} > Log In </button>
+            <button onClick =  {this.logOut} >Log Out </button>
+             <div> This user is { this.state.loggedIn 
+             ? 'logged in'
+             : 'not logged in'}.
+             </div>
+          </div>
+        )
+    }
+} 
+
+
+
+ReactDOM.render (
+    <div>
+    <LoginForm  color='pink' msg= 'Please Login!'/>
+    <Login/>
+    
+    </div>,
+    document.getElementById('root')
+    
+   
+) 
